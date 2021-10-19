7f4963746563ab705732409930bf504aa35c9d88
Francis Shonubi <shonubioladipo@gmail.com>
2021-10-19 20:20:48 +0100

    Finished the Basic Data Structures course

--- basic_data_structures/index.js
+++ basic_data_structures/index.js
@@ -1,25 +1,117 @@
 function filteredArray(arr, elem) {
   let newArr = [];
-  // Only change code below this line
-  for (let i=0;i < arr.length;i++) {
-    // console.log(arr[i]);
-    for (let j=0;j < arr[i].length; j++) {
-      // console.log(arr[i][j]);
-      let element  = (arr[i][j]);
-      // console.log(element);
-      if (elem == element) {
-        // arr[i] = arr[i].splice(j, 1);
-        // arr[i].splice(j, 1);
-        // console.log(i);
-        arr.splice(i,1);
-      }
+  // change code below this line
+
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i].indexOf(elem) == -1) {
+      //Checks every parameter for the element and if is NOT there continues the code
+      newArr.push(arr[i]); //Inserts the element of the array in the new filtered array
     }
   }
-  newArr = [...arr];
 
-  // Only change code above this line
+  // change code above this line
   return newArr;
 }
-
+// change code here to test different cases:
 // console.log(filteredArray([[3, 2, 3], [1, 6, 3], [3, 13, 26], [19, 3, 9]], 3));
-console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));// should return [[10, 8, 3], [14, 6, 23]]
+
+//
+// console.log(filteredArray([[10, 8, 3], [14, 6, 23], [3, 18, 6]], 18));// should return [[10, 8, 3], [14, 6, 23]]
+
+
+let users = {
+  Alan: {
+    age: 27,
+    online: true
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: true
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function isEveryoneHere(userObj) {
+  // Only change code below this line
+   if (
+    userObj.hasOwnProperty("Alan") &&
+    userObj.hasOwnProperty("Jeff") &&
+    userObj.hasOwnProperty("Sarah") &&
+    userObj.hasOwnProperty("Ryan")
+  ) {
+    return true;
+  }
+  return false;
+  // Only change code above this line
+}
+
+// console.log(isEveryoneHere(users));
+//
+//
+
+let dexter = {
+    Alan: {
+      online: false
+    },
+    Jeff: {
+      online: true
+    },
+    Sarah: {
+      online: false
+    }
+  }
+
+function countOnline(usersObj) {
+  // Only change code below this line
+  let counter=0;
+  for (let user in usersObj) {
+    // console.log(user);
+    console.log(usersObj[user].online);
+    if  (usersObj[user].online === true) {
+      counter+=1;
+    }
+    
+  }
+  return counter;
+  // Only change code above this line
+}
+
+// console.log(countOnline(dexter));
+//
+//
+
+let fox = {
+  Alan: {
+    age: 27,
+    online: false
+  },
+  Jeff: {
+    age: 32,
+    online: true
+  },
+  Sarah: {
+    age: 48,
+    online: false
+  },
+  Ryan: {
+    age: 19,
+    online: true
+  }
+};
+
+function getArrayOfUsers(obj) {
+  // Only change code below this line
+  return Object.keys(obj);
+  // Only change code above this line
+}
+
+// console.log(getArrayOfUsers(fox));
+//
+//
