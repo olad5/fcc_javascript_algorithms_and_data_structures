6a698a48c8e715dd7968e003252a685597ad8c0e
Francis Shonubi <shonubioladipo@gmail.com>
2021-10-26 17:27:48 +0100

    Finished Everything be True

---
+++ intermediate_algorithm_scripting/truthCheck.js
@@ -0,0 +1,16 @@
+// Everything Be True
+function truthCheck(collection, pre) {
+  let newArr = [];
+  for ( let n in collection) {
+    // this adds all the Booleans the whole collection.pre
+    newArr.push(Boolean(collection[n][pre]));
+  }
+  return newArr.every(x=> x==true);
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));// true
+// console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));// false
+// console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male", "age": 0}, {"user": "Dipsy", "sex": "male", "age": 3}, {"user": "Laa-Laa", "sex": "female", "age": 5}, {"user": "Po", "sex": "female", "age": 4}], "age"));// false
+// console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true}, {"name": "FastForward", "onBoat": null}], "onBoat"));// false
+// console.log(truthCheck([{"name": "Pete", "onBoat": true}, {"name": "Repeat", "onBoat": true, "alias": "Repete"}, {"name": "FastForward", "onBoat": true}], "onBoat"));// true
+// console.log(truthCheck([{"single": "double"}, {"single": NaN}], "single"));// false
