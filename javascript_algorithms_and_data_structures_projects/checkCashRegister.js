20e2e19b07468a7d06e9999acebdc1b930276178
Francis Shonubi <shonubioladipo@gmail.com>
2021-11-01 14:49:17 +0100

    Completed the Cash Register Project and hence completed the js course

--- javascript_algorithms_and_data_structures_projects/checkCashRegister.js
+++ javascript_algorithms_and_data_structures_projects/checkCashRegister.js
@@ -1,67 +1,53 @@
+// CASH REGISTER
 function checkCashRegister(price, cash, cid) {
-    let currency_units  = {
-            PENNY:'0.01',
-            NICKEL:'0.05',
-            DIME:'0.1',
-            QUARTER:'0.25',
-            ONE:'1',
-            FIVE:'5',
-            TEN:'10',
-            TWENTY:'20',
-            'ONE HUNDRED':'100',
-        };
-  let figures  = Object.values( currency_units).map(i=> Number(i));
-  let cur_names  = Object.keys( currency_units).map(i=> (i));
-  let change = cash - price;
-  let total_cid = cid.map((arr) => arr[1]).reduce((acc, currenValue) => acc + currenValue).toFixed(2);
-   // console.log(cid.map((arr) => arr[1]));
-   let list_avail_cid = (cid.map((arr) => arr[1]));
-  // console.log(list_avail_cid);
-  // this checks if the total_cid is less than change available
-  if (total_cid == change) {
-    console.log(`Change: ${change}`);
-    console.log(`total_cid: ${total_cid}`);
-    return ({
-      status: "CLOSED", change: cid
-    });
+  let change = 100 * (cash - price);
+  let originalCid = JSON.parse(JSON.stringify(cid));
+
+  let availableFunds = 0;
+  
+  // check what happens if you remove 10000, the last figure in the array below
+  let moneyValues = [1, 5, 10, 25, 100, 500, 1000, 2000, 10000];
+  let amtToReturn = [];
+  
+  for (let i = cid.length - 1; i >= 0; i--){
+    let amt = 0;
+    while (moneyValues[i] <= change && cid[i][1] > 0 && change > 0){
+      cid[i][1] -= moneyValues[i]/100; // reduce amount in cid
+      change -= moneyValues[i]; // reduce amount from change
+      amt += moneyValues[i]/100; // keep track of how much money was taken out of cid
+    }
+    if (amt !== 0){
+      // adds record of amount taken out of cid
+      amtToReturn.push([cid[i][0], amt]);
+    }
   }
-  // else if (figures.includes(change) == true && total_cid == change) {
-  // else if (total_cid == change) {
-  // else if (total_cid < change || figures.includes(change) == false) {
-  // else if (total_cid < change || figures.includes(change) == false) {
-  // console.log(total_cid > pay);
-  else if (total_cid < change || list_avail_cid.includes(change) == false) {
-    console.log(`Change: ${change}`);
-    console.log(`total_cid: ${total_cid}`);
+  
+  
+  // if there is still some change left over
+  if (change !== 0){
     return ({
       status: "INSUFFICIENT_FUNDS", change: []
     });
-
   }
-  else {
-    console.log(cid);
-
+  
+  // if there is any money left in cid, it returns amtToReturn
+  for (let j = 0; j < cid.length; j++){
+    if (cid[j][1] > 0){
+      return ({
+          status: "OPEN", change: amtToReturn
+      });
+    }
   }
-  // console.log(cur_names);
-  // return change;
-  // return figures;
-  // return cur_names;
-  // return change;
+  
+  // if register is empty
+  return ({
+      status: "CLOSED", change: originalCid
+  });
+  
 }
-    let currency_units_clone  = {
-            PENNY:'0.01',
-            NICKEL:'0.05',
-            DIME:'0.1',
-            QUARTER:'0.25',
-            ONE:'1',
-            FIVE:'5',
-            TEN:'10',
-            TWENTY:'20',
-            'ONE HUNDRED':'100',
-        };
 
-// in all the situations, total_cid is larger than change
-//
+
+
 console.log(checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]])); // {status: "OPEN", change: [["QUARTER", 0.5]]}
 
 
@@ -74,4 +60,4 @@ console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME"
 console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // {status: "INSUFFICIENT_FUNDS", change: []}
 
 
-// console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
+console.log(checkCashRegister(19.5, 20, [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]])); // {status: "CLOSED", change: [["PENNY", 0.5], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 0], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]}
